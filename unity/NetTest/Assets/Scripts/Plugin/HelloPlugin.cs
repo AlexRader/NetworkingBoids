6795b9ca0f95fb1dae76136f6a691f8078c7c4e3
Alexander Rader <arader@comcast.net>
2018-10-11 11:58:41 -0400

    Committing base unity project for networking boids.

---
+++ unity/NetTest/Assets/Scripts/Plugin/HelloPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//need
+using System.Runtime.InteropServices;
+public class HelloPlugin : MonoBehaviour
+{
+    [DllImport("egp-net-plugin-Unity x64")]
+    static extern int startup();
+
+    [DllImport("egp-net-plugin-Unity x64")]
+    static extern int shutdown();
+
+    [DllImport("egp-net-plugin-Unity x64")]
+    static extern int foo(int bar);
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+    int counter = 0;
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("counter = " + counter.ToString() + ", foo = " + foo(counter++).ToString());
+	}
+}
