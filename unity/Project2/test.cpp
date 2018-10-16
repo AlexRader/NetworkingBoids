614dd0d4b291c3db5ab497cee16cc7df3ec86c71
Alexander Rader <arader@comcast.net>
2018-10-16 18:42:38 -0400

    Ok racknet's in boi

---
+++ unity/Project2/test.cpp
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h> // needed for strlen
+#include "RakNet/RakPeerInterface.h" //include changed to correct directory
+#include "RakNet/MessageIdentifiers.h" //include changed to correct directory
+#include "RakNet/BitStream.h" // include changed to correct directory
+#include "RakNet/RakNetTypes.h"  // MessageID // include changed to correct directory
